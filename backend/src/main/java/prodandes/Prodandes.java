ab6131d25cf90de050eefb5e76545a3c2ed7ac91
JonathanCalderon <jf.calderon1377@uniandes.edu.co>
2015-03-13 23:20:36 -0500

    RF12 v1

--- backend/src/main/java/prodandes/Prodandes.java
+++ backend/src/main/java/prodandes/Prodandes.java
@@ -34,54 +34,123 @@ import javax.ws.rs.core.MediaType;
  * @author jf.molano1587
  */
 public class Prodandes {
+
     public Connection con;
+
     @POST
-    @Path("/metodo")
+    @Path("/registrarPedido")
     public String registrarPedido(List lista) throws Exception {
-        try{
+        try {
             LinkedHashMap lProducto = (LinkedHashMap) lista.get(0);
             LinkedHashMap lCliente = (LinkedHashMap) lista.get(1);
             LinkedHashMap lCantidad = (LinkedHashMap) lista.get(2);
             LinkedHashMap lFechaEsperada = (LinkedHashMap) lista.get(3);
+
+            String nombreProducto = lProducto.get("nombre").toString();
+            int cantidad = (int) lCantidad.get("cantidad");
+
+            int cantidadEnBodega = cantidadProductoEnBodega(nombreProducto);
+
+            abrirConexion();
+            String sql = "select max (id) as MAXIMO from PEDIDO_PRODUCTO";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            int id_pedido = -1;
+            if (rs.next()) {
+                id_pedido = rs.getInt("MAXIMO");
+                
+                //Crear pedido nuevo
+            }
+            cerrarConexion();
+            if (id_pedido == -1) {
+                throw new Exception("Error asignando ID del pedido");
+            }
+            if (cantidadEnBodega >= cantidad) {
+
+                reservarProducto(nombreProducto, cantidad, id_pedido);
+            }
+            else{
+                
+                //Reservar recursos o pedir suministros
+                
+            }
+            st.close();
             return "";
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             return "error";
         }
     }
-    
+
     @POST
     @Path("/cantidadProductoEnBodega")
-    public int cantidadProductoEnBodega(String nombre) throws Exception
-    {
+    public int cantidadProductoEnBodega(String nombre) throws Exception {
         System.out.println("Entrada parámetro cantidadProductoEnBodega");
         System.out.println(nombre);
         abrirConexion();
-        String query = "select count(*) as cuenta from ITEM where NOMBRE_PRODUCTO='"+nombre+"' and ESTADO='Bodega'";
+        String query = "select count(*) as cuenta from ITEM where NOMBRE_PRODUCTO='" + nombre + "' and ESTADO='Bodega'";
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(query);
-        int resp=0;
-        if(rs.next())
-        {
+        int resp = 0;
+        if (rs.next()) {
             resp = rs.getInt("cuenta");
         }
+        st.close();
+        cerrarConexion();
         return resp;
     }
-    
-    public void abrirConexion() throws Exception
-    {
+
+    public void abrirConexion() throws Exception {
         con = null;
         Class.forName("oracle.jdbc.driver.OracleDriver");
         con = DriverManager.getConnection("jdbc:oracle:thin:@157.253.238.224:1531:prod", "ISIS2304271510", "rproxyquark");
     }
-    
-        public void cerrarConexion() throws Exception
-    {
-        if(con!=null)
-        {
-        con.close();
-        con=null;
+
+    public void cerrarConexion() throws Exception {
+        if (con != null) {
+            con.close();
+            con = null;
         }
+    }
+
+    /**
+     * Se reserva la cantidad de ese producto que está en bodega, y se pasa a
+     * estado reservado y se asocia los items con el pedido
+     * @param nombreProducto
+     * @param cantidad
+     * @param id_pedido
+     * @throws Exception 
+     */
+    public void reservarProducto(String nombreProducto, int cantidad, int id_pedido) throws Exception {
+
+        abrirConexion();
+        String query = "select * from ITEM where NOMBRE_PRODUCTO='" + nombreProducto + "' and ESTADO='Bodega'";
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+
+        for (int i = 0; i < cantidad && rs.next(); i++) {
+
+            int id = rs.getInt("ID");
+            String sql2 = "update ITEM set ESTADO='Reservado',ID_PEDIDO=" + id_pedido + " where ID = " + id;
+
+            Statement st2 = con.createStatement();
+            st2.executeUpdate(sql2);
+            st2.close();
+        }
+
+        st.close();
+        cerrarConexion();
+    }
+
+    @POST
+    @Path("/reservarProducto")
+    public void reservarProductoREST(List lista) throws Exception{
+
+        LinkedHashMap lNombreProducto = (LinkedHashMap) lista.get(0);
+        LinkedHashMap lCantidad = (LinkedHashMap) lista.get(1);
+        LinkedHashMap lIdPedido = (LinkedHashMap) lista.get(2);
+        
+        reservarProducto(lNombreProducto.get("nombreProducto").toString()
+                , (int)lCantidad.get("cantidad"), 
+                (int)lIdPedido.get("id_pedido"));
     }
 }
